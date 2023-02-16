003dea849acfa9bfd46b827d020ebf3d813f8c6f
wilder <wilder@aptitudeglobal.com>
2023-02-16 06:19:02 +0100

    code coverage config

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import { Greet } from "./greet"
 
-test('Greet renders correctly',  () =>{
-    render(<Greet />)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument()
-})
+describe('Greet', () => {
+    test('Greet renders correctly',  () =>{
+        render(<Greet />)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument()
+    })
 
-test('should render greet with a name', () => { 
-    render(<Greet name='wilder'/>)
-    const textElement = screen.getByText('hello wilder')
-    expect(textElement).toBeInTheDocument()
+    test('Ggreet renders with a name', () => {
+        render(<Greet name='wilder'/>)
+        const textElement = screen.getByText('hello wilder')
+        expect(textElement).toBeInTheDocument()
+    })
 })
