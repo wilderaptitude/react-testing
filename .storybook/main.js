b3d2bb00889ab1480399de85083b2ffe0319ffd5
wilder <wilder@aptitudeglobal.com>
2023-03-07 00:09:59 +0100

    adds test coverage command

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,8 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage'
   ],
   "framework": "@storybook/react",
   "core": {
