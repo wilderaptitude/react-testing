d9bf16962b3e2bc45f5f39a72bd5380d726a50cd
wilder <wilder@aptitudeglobal.com>
2023-03-17 17:37:54 +0100

    e2e on actions

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,12 @@
+describe('login form', () => {
+  it('should login', () => {
+    cy.visit('http://localhost:6006/iframe.html?args=&id=login--default&viewMode=story')
+    cy.get('input[data-testid="email"]').type('wilder@aptitude.com');
+    cy.get('input[data-testid="password"]').type('password');
+    cy.get('button[type="submit"]').click();
+
+    cy.on('window:alert',(text)=>{
+      expect(text).to.contains('wilder@aptitude.com, password');
+    });
+  })
+})
